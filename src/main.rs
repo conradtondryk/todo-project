e325da7b5c6e0d90adf27c3b2d26b6728bc129fc
Conrad Tondryk <208223989+conradtondryk@users.noreply.github.com>
2025-10-28 16:41:42 +0000

    first init, define enums and structs

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use clap::{Parser, Subcommand};
+use std::env;
+
+#[derive(Parser)]
+#[command(name = "todo")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Add { name: String },
+    Remove { name: String },
+    View { list: Vec<String> },
+}
+
+fn main() {
+    let input = Cli::parse();
+
+    match input.command {
+        Commands::Add { name } => println!("{name} added!"),
+        Commands::Remove { name } => println!("{name} removed!"),
+        Commands::View { list } => print!("{:?}", list),
+    }
+}
