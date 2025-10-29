b5bb456298553e479689fd52b1e759ed9cb395e5
Conrad Tondryk <208223989+conradtondryk@users.noreply.github.com>
2025-10-29 13:21:42 +0000

    read from json, implement view

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::{Parser, Subcommand};
 use serde::Deserialize;
 use serde_json;
-use std::env;
+use std::{env, fs, path::Path};
 
 #[derive(Parser)]
 #[command(name = "todo")]
@@ -14,14 +14,25 @@ struct Cli {
 enum Commands {
     Add { name: String },
     Remove { name: String },
-    View { list: Vec<String> },
+    View,
 }
 
-fn json_editor() {
-    let json_data = 
+#[derive(Deserialize, Debug)]
+struct Task {
+    id: i32,
+    name: String,
+    completed: bool,
+}
 
+fn json_editor() -> Result<(), Box<dyn std::error::Error>> {
+    let path = Path::new("list.json");
 
-
+    let json_data = fs::read_to_string(path).expect("");
+    let user: Task = serde_json::from_str(&json_data)?;
+    println!("{:?}", user.id);
+    println!("{:?}", user.name);
+    println!("{:?}", user.completed);
+    Ok(())
 }
 
 fn main() {
@@ -30,6 +41,6 @@ fn main() {
     match input.command {
         Commands::Add { name } => println!("{name} added!"),
         Commands::Remove { name } => println!("{name} removed!"),
-        Commands::View { list } => print!("{:?}", list),
+        Commands::View => json_editor().unwrap(),
     }
 }
