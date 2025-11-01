59e4aae9c514ee451b9d734b3a0bd1d04b202c77
Conrad Tondryk <208223989+conradtondryk@users.noreply.github.com>
2025-11-01 13:15:26 +0000

    clean up before add features

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ enum Commands {
     Add { name: String },
     Remove { id: usize },
     View,
-    Complete { name: String },
+    Complete { id: usize },
 }
 
 #[derive(Deserialize, Serialize, Debug)]
@@ -70,10 +70,9 @@ impl Commands {
             println!("{}) {}", i + 1, task.name);
         });
     }
-    fn task_complete(tasks: &mut Vec<Task>, id: usize) -> Result<(), Box<dyn std::error::Error>> {
+    fn task_complete(tasks: &mut Vec<Task>, id: usize) {
         tasks.remove(id - 1);
         println!("'{id}' completed!");
-        Ok(())
     }
 }
 
@@ -92,8 +91,8 @@ fn json_editor(command: Commands) -> Result<(), Box<dyn std::error::Error>> {
         Commands::View => {
             Commands::view_tasks(&mut tasks);
         }
-        Commands::Complete { name } => {
-            Commands::task_complete(&mut tasks, &name)?;
+        Commands::Complete { id } => {
+            Commands::task_complete(&mut tasks, id);
             save_tasks(&tasks)?;
         }
     }
