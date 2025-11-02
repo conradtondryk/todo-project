fa5810db250a46c93a807afbedfc3d13393d7913
Conrad Tondryk <208223989+conradtondryk@users.noreply.github.com>
2025-11-02 14:48:51 +0000

    restructure for impl tasks

--- src/main.rs
+++ src/main.rs
@@ -23,44 +23,47 @@ struct Task {
     timestamp: chrono::DateTime<Local>,
     name: String,
 }
+#[derive(Deserialize, Serialize, Debug)]
+#[serde(transparent)]
+struct Tasks(Vec<Task>);
 
-fn load_tasks() -> Result<Vec<Task>, Box<dyn std::error::Error>> {
-    match fs::read_to_string("list.json") {
-        Ok(data) => serde_json::from_str(&data).map_err(Into::into),
-        Err(e) if e.kind() == std::io::ErrorKind::NotFound => Ok(vec![]),
-        Err(e) => Err(e.into()),
+impl Tasks {
+    fn load() -> Result<Self, Box<dyn std::error::Error>> {
+        match fs::read_to_string("list.json") {
+            Ok(data) => serde_json::from_str(&data).map_err(Into::into),
+            Err(e) if e.kind() == std::io::ErrorKind::NotFound => Ok(Tasks(vec![])),
+            Err(e) => Err(e.into()),
+        }
     }
-}
 
-fn save_tasks(tasks: &[Task]) -> Result<(), Box<dyn std::error::Error>> {
-    serde_json::to_writer(File::create("list.json")?, tasks)?;
-    Ok(())
-}
+    fn save(&self) -> Result<(), Box<dyn std::error::Error>> {
+        serde_json::to_writer(File::create("list.json")?, &self.0)?;
+        Ok(())
+    }
 
-impl Commands {
-    fn add_task(tasks: &mut Vec<Task>, name: &str) -> Result<(), Box<dyn std::error::Error>> {
+    fn add(&mut self, name: &str) -> Result<(), Box<dyn std::error::Error>> {
         if name.is_empty() {
             return Err("Name cannot be empty.".into());
         }
-        tasks.push(Task {
+        self.0.push(Task {
             name: name.to_string(),
             timestamp: Local::now(),
         });
         println!("{name} added!");
         Ok(())
     }
-    fn remove_task(tasks: &mut Vec<Task>, id: usize) -> Result<(), Box<dyn std::error::Error>> {
+    fn remove(&mut self, id: usize) -> Result<(), Box<dyn std::error::Error>> {
         if id == 0 {
             return Err("ID cannot be 0.".into());
-        } else if id > tasks.len() {
+        } else if id > self.0.len() {
             return Err("ID is out of range.".into());
         }
-        let removed = tasks.remove(id - 1);
+        let removed = self.0.remove(id - 1);
         println!("{} removed! ({})", id, removed.name);
         Ok(())
     }
-    fn view_tasks(tasks: &mut [Task]) {
-        tasks.iter().enumerate().for_each(|(i, task)| {
+    fn view(&self) {
+        self.0.iter().enumerate().for_each(|(i, task)| {
             println!(
                 "{} ({}) {}",
                 task.timestamp.format("%d/%m/%Y %H:%M"),
@@ -72,19 +75,19 @@ impl Commands {
 }
 
 fn json_editor(command: Commands) -> Result<(), Box<dyn std::error::Error>> {
-    let mut tasks: Vec<Task> = load_tasks()?;
+    let mut tasks = Tasks::load()?;
 
     match command {
         Commands::Add { name } => {
-            Commands::add_task(&mut tasks, &name)?;
-            save_tasks(&tasks)?;
+            tasks.add(&name)?;
+            tasks.save()?;
         }
         Commands::Remove { id } => {
-            Commands::remove_task(&mut tasks, id)?;
-            save_tasks(&tasks)?;
+            tasks.remove(id)?;
+            tasks.save()?;
         }
         Commands::View => {
-            Commands::view_tasks(&mut tasks);
+            tasks.view();
         }
     }
     Ok(())
